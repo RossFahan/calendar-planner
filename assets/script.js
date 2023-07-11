5142642bf1bf66765e1605900bb42f72f25cd01f
Ross F <rfahan@gmail.com>
2023-07-11 13:32:00 -0600

    added id tags to text area to fix issue shown in console

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,34 @@
-$(function() {
+$(document).ready(function() {
   // Add a listener for click on the save button
-  $(".saveBtn").on("click", function() {
+  $(document).on("click", ".saveBtn", function() {
     var hour = $(this).parent().attr("id"); // Get the id of the containing time-block
     var description = $(this).siblings(".description").val(); // Get the user input
 
     localStorage.setItem(hour, description); // Save the user input in local storage
   });
 
-// Get the current hour using Day.js
-  var currentHour = dayjs().hour(); 
+  // Get the current hour using Day.js
+  var currentHour = dayjs().hour();
 
-  //use jQuery .each method to iterate each time block and apply the correct class (past, present, future)
- $(".time-block").each(function() {
+  // Use jQuery .each method to iterate each time block and apply the correct class (past, present, future)
+  $(".time-block").each(function() {
     var hour = parseInt($(this).attr("id").split("-")[1]); // Extract the hour from the id
 
     if (hour < currentHour) {
-      $(this).removeClass("present future").addClass("past"); 
+      $(this).removeClass("present future").addClass("past");
     } else if (hour === currentHour) {
       $(this).removeClass("past future").addClass("present");
     } else {
       $(this).removeClass("past present").addClass("future");
     }
   });
-  
-  // Get user input saved in localStorage and set the values of the corresponding textarea elements
+
+  // Get  user input saved in localStorage and set the values of the corresponding textarea elements
   for (var i = 9; i <= 17; i++) {
     var event = localStorage.getItem("hour-" + i); // Get the saved event from local storage
 
     if (event) {
-      $("#hour-" + i).children(".description").val(event); // Set the value of the textarea
+      $("#hour-" + i).find(".description").val(event); // Set the value of the textarea
     }
   }
 
