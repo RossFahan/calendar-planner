91c696b2b70b7df00ced9cc2d56b8984bea73b44
Ross F <rfahan@gmail.com>
2023-07-11 19:40:59 -0600

    added loop to Get user input saved in localStorage and set the values of the corresponding textarea elements

--- assets/script.js
+++ assets/script.js
@@ -23,20 +23,14 @@ $(function() {
     }
   });
   
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  //1. getItem for localStorage to pull previously stored content
-      //a. separate key in localStorage for each hour
-      //b create a for loop for hour 9-18 
-           //check localStorage eithin the loop
-           //if there is storage (value) 
-               //  inside the text area of corresponding hour
-               //use id selector for the text area
+  // Get user input saved in localStorage and set the values of the corresponding textarea elements
+  for (var i = 9; i <= 17; i++) {
+    var event = localStorage.getItem("hour-" + i); // Get the saved event from local storage
 
-           // else no content do nothing
-
+    if (event) {
+      $("#hour-" + i).children(".description").val(event); // Set the value of the textarea
+    }
+  }
 
   // Display the current date in the header of the page
   var currentDate = dayjs().format("dddd, MMMM D, YYYY"); // Get the current date using Day.js
